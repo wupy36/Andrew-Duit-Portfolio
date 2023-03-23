0e5a1ec0e7886464d554199c771978c57c6e42eb
Andrew Duit <wupy36@gmail.com>
2023-03-23 16:55:25 -0500

    updating project structure made an op on creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const { toggleColorMode } = useColorMode()
-  const formBackground = useColorModeValue("gray.100", "gray.700")
+  const formBackground = useColorModeValue("gray.200", "gray.700")
   return (
     <Flex height="100vh" alignItems="center" justifyContent="Center">
         <Flex direction="column" background={formBackground} p={12} rounded={6}>
           <Heading mb={6}>Log in</Heading>
           <Input placeholder='andrewduit@gmail.com' variant="filled" mb={3} type="email"/>
           <Input placeholder='******' variant="filled" mb={6} type="password"/>
-          <Button mb={6} colorScheme="red"> Log in</Button>
+          <Button mb={6} colorScheme="purple"> Log in</Button>
           <Button onClick={toggleColorMode}>Toggle Color</Button>
         </Flex>
     </Flex>
